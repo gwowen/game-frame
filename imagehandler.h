0d935f6989905553d670036035efe486d6027445
Gareth <gareth.w.owen@gmail.com>
2014-01-17 01:41:20 +0000

    Wrap everything in a nice package to preserve it

---
+++ imagehandler.h
@@ -0,0 +1,18 @@
+/* 
+ * File:   imagehandler.h
+ * Author: gareth
+ *
+ * Created on 16 January 2014, 20:24
+ */
+
+#ifndef IMAGEHANDLER_H
+#define	IMAGEHANDLER_H
+
+class imageHandler {
+    
+};
+
+
+
+#endif	/* IMAGEHANDLER_H */
+
